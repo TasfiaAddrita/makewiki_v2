b95f86c133f289aacdfb749072a1a79b790142d3
TasfiaAddrita <tasfia.addrita@gmail.com>
2019-11-13 14:18:49 -0800

    completed new page functionality, need to style new_page form

--- wiki/views.py
+++ wiki/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 from wiki.models import Page
-
+from wiki.forms import PageForm
 
 class PageListView(ListView):
     """ Renders a list of all Pages. """
@@ -11,7 +13,7 @@ class PageListView(ListView):
 
     def get(self, request):
         """ GET a list of Pages. """
-        pages = self.get_queryset().all()
+        pages = self.get_queryset().all().order_by('-created')[::1]
         return render(request, 'list.html', {
           'pages': pages
         })
@@ -26,3 +28,13 @@ class PageDetailView(DetailView):
         return render(request, 'page.html', {
           'page': page
         })
+
+class PageCreateView(CreateView):
+    template_name = 'new_page.html'
+    form_class = PageForm
+    success_url = '/'
+    queryset = Page.objects.all()
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
